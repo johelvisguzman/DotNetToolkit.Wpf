7ffb8bce0c91ead57637e7ef42c0bd1ace90ae0a
Johelvis Guzman <johelvisguzman@gmail.com>
2018-02-04 05:25:32 -0500

    Added a new FluentValidation demo project

---
+++ samples/DotNetToolkit.Wpf.FluentVValidation.Demo/Infrastructure/FluentExplicitValidatableViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNetToolkit.Wpf.FluentVValidation.Demo.Infrastructure
+{
+    using FluentValidation;
+    using Mvvm;
+    using System;
+    using System.Threading.Tasks;
+
+    public abstract class FluentExplicitValidatableViewModelBase<TValidator> : ViewModelBase where TValidator : IValidator
+    {
+        private readonly IValidator _validator;
+
+        protected FluentExplicitValidatableViewModelBase()
+        {
+            _validator = Activator.CreateInstance<TValidator>();
+        }
+
+        public async Task<bool> ValidateAsync()
+        {
+            ClearErrors();
+
+            var result = await _validator.ValidateAsync(this);
+            if (result == null)
+                return false;
+
+            foreach (var error in result.Errors)
+            {
+                SetError(error.PropertyName, error.ErrorMessage);
+            }
+
+            return result.IsValid;
+        }
+    }
+}
