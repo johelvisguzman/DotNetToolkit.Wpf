67600b7872a639dbd06fb2ae1ee34e6fcb1635e5
Johelvis Guzman <johelvisguzman@gmail.com>
2018-02-22 10:12:11 -0500

    Closes #13

---
+++ samples/DotNetToolkit.Wpf.Demo/Views/BusyIndicatorExampleView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetToolkit.Wpf.Demo.Views
+{
+    /// <summary>
+    /// Interaction logic for BusyIndicatorExampleView.xaml
+    /// </summary>
+    public partial class BusyIndicatorExampleView
+    {
+        public BusyIndicatorExampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/DotNetToolkit.Wpf/Controls/BusyIndicator.cs
@@ -0,0 +1,58 @@
+﻿namespace DotNetToolkit.Wpf.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Shapes;
+
+    [StyleTypedProperty(Property = "OverlayStyle", StyleTargetType = typeof(Rectangle))]
+    public class BusyIndicator : ContentControl
+    {
+        #region Constructors
+
+        static BusyIndicator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyIndicator), new FrameworkPropertyMetadata(typeof(BusyIndicator)));
+        }
+
+        #endregion
+
+        #region Dependency Properties
+
+        /// <summary>
+        /// The is busy property
+        /// </summary>
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register(
+            "IsBusy",
+            typeof(bool),
+            typeof(BusyIndicator));
+
+        /// <summary>
+        /// The overlay style property
+        /// </summary>
+        public static readonly DependencyProperty OverlayStyleProperty = DependencyProperty.Register(
+            "OverlayStyle",
+            typeof(Style),
+            typeof(BusyIndicator),
+            new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is busy.
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return (bool)GetValue(IsBusyProperty); }
+            set { SetValue(IsBusyProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the overlay style.
+        /// </summary>
+        public Style OverlayStyle
+        {
+            get { return (Style)GetValue(OverlayStyleProperty); }
+            set { SetValue(OverlayStyleProperty, value); }
+        }
+
+        #endregion
+    }
+}
