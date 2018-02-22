d6d7c31800dd96904888a09af56b99f84a48010a
Johelvis Guzman <johelvisguzman@gmail.com>
2018-02-22 10:12:11 -0500

    Ported over Silverlight4 BusyIndicator

---
+++ src/DotNetToolkit.Wpf/Converters/ProgressBarWidthConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotNetToolkit.Wpf.Converters
+{
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ProgressBarWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var contentWidth = (double)values[0];
+            var parentMinWidth = (double)values[1];
+
+            return Math.Max(contentWidth, parentMinWidth);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
