77c2467da0fafa76011131d15c089eb9b0475ca7
Johelvis Guzman <johelvisguzman@gmail.com>
2018-02-23 09:06:02 -0500

    Add metro style to the busy indicator

---
+++ samples/DotNetToolkit.Wpf.Metro.Demo/Views/BusyIndicatorExampleView.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace DotNetToolkit.Wpf.Metro.Demo.Views
+{
+    using System;
+    using System.Threading;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for BusyIndicatorExampleView.xaml
+    /// </summary>
+    public partial class BusyIndicatorExampleView
+    {
+        public BusyIndicatorExampleView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int busySeconds = (int)BusySeconds.SelectedItem;
+            int delayMilliseconds = (int)DelayMilliseconds.SelectedItem;
+            SampleIndicator.DisplayAfter = TimeSpan.FromMilliseconds(delayMilliseconds);
+
+            // Simulate a long-running task by sleeping on a worker thread
+            DataContext = true;
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                Thread.Sleep(busySeconds * 1000);
+                Dispatcher.BeginInvoke(new Func<object>(() => DataContext = false));
+            });
+        }
+    }
+}
